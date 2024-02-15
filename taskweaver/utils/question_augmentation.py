f415b7d8182c695793ce880d331453b30040ea77
Darian Raad <darian.raad@honu.ai>
2024-02-15 17:12:11 +0000

    root dir

--- taskweaver/utils/question_augmentation.py
+++ taskweaver/utils/question_augmentation.py
@@ -1,5 +1,8 @@
 import yaml
+from pathlib import Path
 
-with open('../project/questions.yaml', 'r') as file:
+ROOT_DIR = Path(__file__).resolve().parents[3]
+
+with open(ROOT_DIR / 'project/questions.yaml', 'r') as file:
     question_map = yaml.safe_load(file)
 
